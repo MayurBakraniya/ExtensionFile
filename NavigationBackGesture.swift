c80993768a6bffb439fb7d795cb681ca784ca3a0
Mayur Bakraniya <72742503+MayurBakraniya@users.noreply.github.com>
2021-07-01 10:11:46 +0530

    Add files via upload

---
+++ NavigationBackGesture.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationBackGesture.swift
+//
+//  Created by Adsum MAC 3 on 01/07/21.
+//
+
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
